d16f3d561e20a4f25a02d8400321a156b8eca3db
LeonardoContino <leoconti2000@hotmail.it>
2023-02-17 16:36:16 +0100

    stampato in pagina

--- index.php
+++ index.php
@@ -29,8 +29,37 @@ $movie1 = new Movie("Il Signore degli anelli", "eng", "leo, Marco, Giacomo, Giov
 
 $movie2 = new Movie("The Hobbit", "eng", "gino, Marco, stefano, Giovanni","2:45h", "Fantasy");
 
-echo $movie1->getFullMovie();
-echo $movie2->getFullMovie();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+       <?php ?> 
+       <div>
+       <h2> <?=$movie1->title ?></h2>
+        <h3><?=$movie1->language ?></h3> <span><?=$movie1->duration ?></span>
+        <p><?=$movie1->cast ?></p>
+        <p><?=$movie1->genre ?></p>
+       </div>
+
+       <div>
+       <h2> <?=$movie2->title ?></h2>
+        <h3><?=$movie2->language ?></h3> <span><?=$movie1->duration ?></span>
+        <p><?=$movie2->cast ?></p>
+        <p><?=$movie2->genre ?></p>
+       </div>
+
+       <?php ?> 
+    </section>
+</body>
+</html>
 
 
 
